ad26977a3429bd15f3e730135f0bcbb7692d13ce
Lari Hotari <lhotari@pivotal.io>
2015-02-02 15:34:32 -0500

    add base styles to plugins-template.html

--- site/src/site/pages/plugins-template.groovy
+++ site/src/site/pages/plugins-template.groovy
@@ -1,4 +1,4 @@
 layout 'layouts/main.groovy', true,
         pageTitle: 'The Grails Framework - Plugins',
         mainContent: contents { yieldUnescaped '<!--MAINCONTENT-->' },
-        baseUri: '/', noBaseStyles: true, noBaseScripts: true
+        baseUri: '/', noBaseScripts: true

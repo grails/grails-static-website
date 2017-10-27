d0e02504d529efbbc58b6a5ac0c05814890695f5
sheehan <mr.sheehan@gmail.com>
2017-10-26 22:24:50 -0500

    resolve grails/grails3-plugins#13 redirect to new page

--- site/src/site/pages/plugins.groovy
+++ site/src/site/pages/plugins.groovy
@@ -1,2 +1,11 @@
-layout 'layouts/iframeplugins.groovy', true,
-        pageTitle: "The Grails Framework - Grails 3 Plugins"
+html {
+    head {
+        meta 'http-equiv': 'refresh', content: '2;url=http://plugins.grails.org/'
+        title 'Page Moved'
+    }
+    body {
+        span 'This page has moved. Click '
+        a href: 'http://plugins.grails.org/', 'here'
+        span ' to go to the new page.'
+    }
+}

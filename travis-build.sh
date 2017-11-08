88d8b9ce9010bb07aa444ae5ca5c62235663a2a8
sdelamo <sergio.delamo@softamo.com>
2017-11-08 07:00:18 +0100

    Improve readme docs

--- travis-build.sh
+++ travis-build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+EXIT_STATUS=0
+
 git config --global user.name "$GIT_NAME"
 git config --global user.email "$GIT_EMAIL"
 git config --global credential.helper "store --file=~/.git-credentials"

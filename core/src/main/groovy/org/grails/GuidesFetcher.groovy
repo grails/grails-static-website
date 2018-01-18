1ace047a803c17895a646c80eba3d3a7c4db2a00
Sergio del Amo <sergio.delamo@softamo.com>
2018-01-18 07:52:52 +0100

    SimpleDateFormat to format guide publication date
    See https://github.com/grails/grails-static-website/pull/83

--- core/src/main/groovy/org/grails/GuidesFetcher.groovy
+++ core/src/main/groovy/org/grails/GuidesFetcher.groovy
@@ -5,6 +5,9 @@ import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
 import org.grails.model.Guide
 
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+
 @CompileStatic
 class GuidesFetcher {
 
@@ -14,6 +17,7 @@ class GuidesFetcher {
     static List<Guide> fetchGuides() {
         URL url = new URL(GUIDES_JSON)
         def jsonArr = new JsonSlurper().parseText(url.text)
+        DateFormat dateFormat = new SimpleDateFormat('dd MMM yyyy', Locale.US)
         List<Guide> guides = jsonArr.collect {
             Guide guide = new Guide(authors: it.authors as List,
                     category: it.category,
@@ -24,7 +28,7 @@ class GuidesFetcher {
                     tags: it.tags as List
             )
             if ( it.publicationDate ) {
-                guide.publicationDate = Date.parse('dd MMM yyyy', it.publicationDate)
+                guide.publicationDate = dateFormat.parse(it.publicationDate as String)
             }
             guide
         }

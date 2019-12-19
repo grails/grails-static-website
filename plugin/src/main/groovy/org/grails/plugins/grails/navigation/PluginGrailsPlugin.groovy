d50e80fd0d2f731474c0cdfe16e3ac651f213fa9
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-19 12:09:04 +0100

    def grailsVersion = "4.0.1 > *"

--- plugin/src/main/groovy/org/grails/plugins/grails/navigation/PluginGrailsPlugin.groovy
+++ plugin/src/main/groovy/org/grails/plugins/grails/navigation/PluginGrailsPlugin.groovy
@@ -7,7 +7,7 @@ import groovy.transform.CompileStatic
 class PluginGrailsPlugin extends Plugin {
 
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.3.0 > *"
+    def grailsVersion = "4.0.1 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
         "grails-app/views/error.gsp"

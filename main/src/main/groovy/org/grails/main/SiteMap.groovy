323141ac2c813b50e7774f099bcb61659b9e7c43
Sergio del Amo <sergio.delamo@softamo.com>
2019-12-19 10:06:04 +0100

    remove commented out event

--- main/src/main/groovy/org/grails/main/SiteMap.groovy
+++ main/src/main/groovy/org/grails/main/SiteMap.groovy
@@ -108,12 +108,7 @@ class SiteMap {
                     location: 'Copenhagen, Denmark',
                     dates: 'May/June',
                     about: '''Groovy, Grails, and related technologies have seen astounding growth in interest and adoption over the past few years, and with good reason. GR8Conf is a series of conferences founded to spread the word worldwide. The 2018 GR8Conf Europe is celebrating its 10th year, and it's expected to be a blast. As in 2017 the conference had a DevOps day, this year DevOps topics will be mixed with the rest of the topics. GR8Conf is an independent, affordable series of conferences and covers All Things Groovy'''),
-//            new Event(image: 'confs/gr8confus.png',
-//                    href: 'http://gr8conf.us/',
-//                    title: 'GR8Conf US 2018',
-//                    location: 'Minneapolis, MN, USA',
-//                    dates: 'July 25 - July 27, 2018',
-//                    about: '''Groovy, Grails, and related technologies have seen astounding growth in interest and adoption over the past few years, and with good reason. GR8Conf is a series of conferences founded to spread the word worldwide. GR8Conf is an independent, affordable series of conferences and covers All Things Groovy'''),
+
 
     ]
 

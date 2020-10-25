c98b918c3f698b2b949311887b32b8e900a636a2
Puneet Behl <behlp@objectcomputing.com>
2020-10-26 03:04:40 +0530

    Update release.sh
    Correctly format YAML

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
 DATE=`date +'%b %d, %Y'`
 echo "    -" >> conf/releases.yml
-echo "      version: $1" >> conf/releases.yml
-echo "      publicationDate: $DATE" >> conf/releases.yml
+echo "    version: $1" >> conf/releases.yml
+echo "    publicationDate: $DATE" >> conf/releases.yml

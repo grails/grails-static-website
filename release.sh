92b1a9cbd2823ebb5b14861a30f2bcb258d6aa26
Puneet Behl <behlp@objectcomputing.com>
2021-04-08 16:06:01 +0530

    Fixed release.sh script

--- release.sh
+++ release.sh
@@ -1,4 +1,5 @@
 DATE=`date +'%b %d, %Y'`
-echo "    -" >> conf/releases.yml
+echo "  -" >> conf/releases.yml
 echo "    version: $1" >> conf/releases.yml
 echo "    publicationDate: $DATE" >> conf/releases.yml
+echo ""

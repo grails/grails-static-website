39b9b84661b307c0f067abbe0c4bd8688cd75213
Sergio del Amo <sergio.delamo@softamo.com>
2022-01-20 15:52:55 +0100

    update copyright notice

--- buildSrc/src/main/java/org/grails/airtable/AirtableClient.java
+++ buildSrc/src/main/java/org/grails/airtable/AirtableClient.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017-2020 original authors
+ * Copyright 2017-2022 original authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -31,7 +31,8 @@ import java.util.List;
  * @author Sergio del Amo
  * @since 1.0.0
  */
-@Client(value = "${airtable.url:`" + AirtableConfigurationProperties.DEFAULT_URL + "`}", errorType = AirtableApiError.class)
+@Client(value = "${airtable.url:`" + AirtableConfigurationProperties.DEFAULT_URL + "`}",
+        errorType = AirtableApiError.class)
 public interface AirtableClient extends AirtableApi {
 
     @Override

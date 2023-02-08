acadb1852f3ec6969e42809948869a2f6134d34f
Jason Schindler <schindlerj@objectcomputing.com>
2023-02-08 12:35:55 -0600

    Fixes documentation links for past non-single page user guides and API reference

--- buildSrc/src/main/groovy/org/grails/documentation/DocumentationPage.groovy
+++ buildSrc/src/main/groovy/org/grails/documentation/DocumentationPage.groovy
@@ -91,21 +91,21 @@ class DocumentationPage {
 
                         div(class: 'versionselector') {
                             h4 'Single Page - User Guide'
-                            select(onchange: "window.location.href='https://grails.org/doc/' + this.value + '/guide/single.html'") {
+                            select(onchange: "window.location.href='https://docs.grails.org/' + this.value + '/guide/single.html'") {
                                 option 'Select a version'
                                 mkp.yield('[%versions]')
                             }
                         }
                         div(class: 'versionselector') {
                             h4 'User Guide'
-                            select(onchange: "window.location.href=&apos;https://grails.org/doc/&apos; + this.value") {
+                            select(onchange: "window.location.href='https://docs.grails.org/' + this.value") {
                                 option 'Select a version'
                                 mkp.yield('[%versions]')
                             }
                         }
                         div(class: 'versionselector') {
                             h4 'API Reference'
-                            select(onchange: "window.location.href=&apos;https://grails.org/doc/&apos; + this.value + &apos;/api&apos;") {
+                            select(onchange: "window.location.href='https://docs.grails.org/' + this.value + '/api'") {
                                 option 'Select a version'
                                 mkp.yield('[%versions]')
                             }

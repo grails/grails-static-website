8fc0a7f01c552f5fb0e9c71046c5c3c115f0b8aa
Puneet Behl <behlp@objectcomputing.com>
2023-08-31 00:04:36 +0530

    Update plugins-search.js
    Improvement to JS
    
    * Use const and let instead of var
    * Remove duplicate serach response
    * Code Reformatting and simplifying the logic
    * Use consistent label for "Plugin Filtered By: "

--- assets/javascripts/plugins-search.js
+++ assets/javascripts/plugins-search.js
@@ -1,11 +1,8 @@
-var queryInputFieldId = "query";
-var mobileQueryInputFieldId = "mobilequery";
-var allGuides = new Array();
-var guideClassName = "plugin";
-var multiguideClassName = "multiguide";
-
-
-var elementsClassNames = new Array();
+const queryInputFieldId = "query";
+const mobileQueryInputFieldId = "mobilequery";
+const allGuides = [];
+const guideClassName = "plugin";
+const elementsClassNames = [];
 elementsClassNames.push('plugins');
 elementsClassNames.push('tagcloud');
 elementsClassNames.push('labels');
@@ -14,68 +11,73 @@ elementsClassNames.push('tagsbytopic');
 elementsClassNames.push('plugin');
 
 onload = function () {
-    var elements = document.getElementsByClassName(guideClassName);
-    for ( var i = 0; i < elements.length; i++ ) {
-        var element = elements[i];
-        var name = element.getElementsByClassName('name')
-        var desc = element.getElementsByClassName('desc')
-        var owner = element.getElementsByClassName('owner')
-        var labels = element.getElementsByClassName('label')
+    const elements = document.getElementsByClassName(guideClassName);
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
+        const name = element.getElementsByClassName('name');
+        const desc = element.getElementsByClassName('desc');
+        const owner = element.getElementsByClassName('owner');
+        const labels = element.getElementsByClassName('label');
 
-        var description = desc[0].textContent
+        const description = desc[0]?.textContent;
 
-        var guide = {desc: description, name: name[0].textContent, owner: owner[0].textContent,labels: labelsAtPlugin(labels) }; /* */
+        const guide = {
+            desc: description,
+            name: name[0]?.textContent,
+            owner: owner[0]?.textContent,
+            labels: labelsAtPlugin(labels)
+        };
 
         allGuides.push(guide);
     }
 
     if (document.getElementById(queryInputFieldId)) {
-        var e = document.getElementById(queryInputFieldId);
+        const e = document.getElementById(queryInputFieldId);
         e.oninput = onQueryChanged;
         e.onpropertychange = e.oninput; // for IE8
     }
-    if ( document.getElementById(mobileQueryInputFieldId) ) {
-        var e = document.getElementById(mobileQueryInputFieldId);
+    if (document.getElementById(mobileQueryInputFieldId)) {
+        const e = document.getElementById(mobileQueryInputFieldId);
         e.oninput = onQueryChanged;
         e.onpropertychange = e.oninput; // for IE8
     }
 }
 
 function hideElementsToDisplaySearchResults() {
-    for ( var i = 0; i < elementsClassNames.length; i++) {
-        var className = elementsClassNames[i];
+    for (let i = 0; i < elementsClassNames.length; i++) {
+        const className = elementsClassNames[i];
         hideElementsByClassName(className);
     }
 }
 
 function showElementsToDisplaySearchResults() {
-    for (var i = 0; i < elementsClassNames.length; i++) {
-        var className = elementsClassNames[i];
+    for (let i = 0; i < elementsClassNames.length; i++) {
+        const className = elementsClassNames[i];
         showElementsByClassName(className);
     }
 }
 
 function hideElementsByClassName(className) {
-    var elements = document.getElementsByClassName(className);
-    for ( var i = 0; i < elements.length; i++ ) {
-        var element = elements[i];
+    const elements = document.getElementsByClassName(className);
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
         element.style.display = "none";
     }
 }
 
 function showElementsByClassName(className) {
-    var elements = document.getElementsByClassName(className);
-    for ( var i = 0; i < elements.length; i++ ) {
-        var element = elements[i];
+    const elements = document.getElementsByClassName(className);
+    for (let i = 0; i < elements.length; i++) {
+        const element = elements[i];
         element.style.display = "block";
     }
 }
 
 function labelsAtPlugin(element) {
 
-    var labels = [];
-    for (var y=0; y< element.length; y++){
-            labels.push(element[y].textContent)
+    const labels = [];
+    for (let y = 0; y < element.length; y++) {
+        labels.push(element[y].textContent)
     }
     return labels;
 }
@@ -94,27 +96,26 @@ function labelsAtPlugin(element) {
 //    }
 
 function onQueryChanged() {
-    var query = queryValue();
+    let query = queryValue();
     query = query.trim();
-    if ( query === '' ) {
+    if (query === '') {
         showElementsToDisplaySearchResults();
         document.getElementById("searchresults").innerHTML = "";
         return;
     }
 
-    var matchingGuides = [];
-    if ( query !== '' ) {
-        for (var i = 0; i < allGuides.length; i++) {
-            var guide = allGuides[i];
-            if ( doesGuideMatchesQuery(guide, query) ) {
+    const matchingGuides = [];
+    if (query !== '') {
+        for (let i = 0; i < allGuides.length; i++) {
+            const guide = allGuides[i];
+            if (doesGuideMatchesQuery(guide, query)) {
                 matchingGuides.push(guide);
             }
         }
     }
-    if ( matchingGuides.length > 0 ) {
+    if (matchingGuides.length > 0) {
         hideElementsToDisplaySearchResults();
-        var html = renderGuideGroup(matchingGuides, query);
-        document.getElementById("searchresults").innerHTML = html;
+        document.getElementById("searchresults").innerHTML = renderGuideGroup(matchingGuides);
 
     } else {
         document.getElementById("searchresults").innerHTML = "<div class='guidegroup'><div class='guidegroupheader'><h2>No results found</h2></div></div>";
@@ -122,8 +123,8 @@ function onQueryChanged() {
 }
 
 function doesTagsMatchesQuery(tags, query) {
-    for ( var x = 0; x < tags.length; x++) {
-        var tag = tags[x];
+    for (let x = 0; x < tags.length; x++) {
+        const tag = tags[x];
         if (tag.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
             return true;
         }
@@ -132,15 +133,15 @@ function doesTagsMatchesQuery(tags, query) {
 }
 
 function doesTitleMatchesQuery(title, query) {
-    if (title !== undefined ){
-        if (title.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
+    if (title !== undefined) {
+        if (title.toLowerCase().includes(query.toLowerCase())) {
             return true;
         }
     }
 }
 
 function doesOwnerMatchesQuery(owner, query) {
-    if (owner !== undefined ){
+    if (owner !== undefined) {
         if (owner.toLowerCase().indexOf(query.toLowerCase()) !== -1) {
             return true;
         }
@@ -148,23 +149,15 @@ function doesOwnerMatchesQuery(owner, query) {
 }
 
 function doesGuideMatchesQuery(guide, query) {
-    if (doesTitleMatchesQuery(guide.name, query)) {
-        return true;
-    }
-    if(doesOwnerMatchesQuery(guide.owner, query)){
-        return true;
-    }
-    if(doesTagsMatchesQuery(guide.labels, query)) {
-            return true;
-        }
-
-    return false;
+    return doesTitleMatchesQuery(guide.name, query) ||
+        doesOwnerMatchesQuery(guide.owner, query) ||
+        doesTagsMatchesQuery(guide.labels, query);
 }
 
 function queryValue() {
-    var value = document.getElementById(queryInputFieldId).value;
+    let value = document.getElementById(queryInputFieldId).value;
     value = value.trim();
-    if ( value === '' ) {
+    if (value === '') {
         if (document.getElementById(mobileQueryInputFieldId)) {
             value = document.getElementById(mobileQueryInputFieldId).value;
             value = value.trim();
@@ -174,34 +167,31 @@ function queryValue() {
     return value;
 }
 
-function renderGuideGroup(guides, query) {
-    var html = "";
-
-    html += "  <div class='guidegroupheader'>";
-    html += "    <h2>Guides Filtered by: " + queryValue() + "</h2>";
-    html += "  </div>";
+function renderGuideGroup(guides) {
+    let html = "";
+    html += "  <h3 class=\"columnheader\">Plugins Filtered by: " + queryValue() + "</h3>";
     html += "  <ul>";
-    for ( var i = 0; i < guides.length; i++ ) {
-        html += "    " + renderGuideAsHtmlLi(guides[i], query);
+    for (let i = 0; i <= guides.length-1; i++) {
+        html += "    " + renderGuideAsHtmlLi(guides[i]);
     }
     html += "  </ul>";
 
     return html;
 }
 
-function renderGuideAsHtmlLi(guide, query) {
-    var html = "<li class='plugin'>";
+function renderGuideAsHtmlLi(guide) {
+    let html = "<li class='plugin'>";
     html += "<h3 class='name'><a href=>" + guide.name + "</a> </h3>";
     html += "<p class='desc'>" + guide.desc + "</p>"
-    html += "<a href=/plugins/owners/" + guide.owner +".html>"+guide.owner+ "</a>";
+    html += "<a href=/plugins/owners/" + guide.owner + ".html>" + guide.owner + "</a>";
 
     html += "<ul class='labels'>";
-        for (var i = 0; i < guide.labels.length; i++) {
-            var label = guide.labels[i].trim()
-            html += "<li class='label'>";
-            html += "<a href=\"/plugins/tags/"+label+".html\">"+label+ "</a>";
-            html += "</li>"
-        }
+    for (let i = 0; i < guide.labels.length; i++) {
+        const label = guide.labels[i]?.trim();
+        html += "<li class='label'>";
+        html += "<a href=\"/plugins/tags/" + label + ".html\">" + label + "</a>";
+        html += "</li>"
+    }
     html += "</ul>"
     html += "</li>"
     return html;
